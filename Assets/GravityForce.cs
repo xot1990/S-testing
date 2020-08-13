5fed6adfdd9fa6ba42bedc0436aa22542def2ff3
Анунах великий <guk.012v@gmail.com>
2020-08-13 22:57:49 +0300

    testing gravity

--- Assets/GravityForce.cs
+++ Assets/GravityForce.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class GravityForce : MonoBehaviour
 {
     
-    float GravityConst = 6.67430f * Mathf.Pow(10, -11);
+    float GravityConst = 0.1f;
     Rigidbody2D body;
     public Vector2 StartForce;
-    
+    public float forses;
 
     void Start()
     {
@@ -29,12 +29,13 @@ public class GravityForce : MonoBehaviour
             if (gameObject != Obj)
             {
                 float dist = Vector3.Distance(transform.position, Obj.transform.position);
+                
                 Vector2 dest = Obj.transform.position - transform.position;
                 Vector2 d = new Vector2(0, 1);
                 dest.Normalize();
-                Vector2 forse = dest * GravityConst * ((body.mass * Obj.GetComponent<Rigidbody2D>().mass)*100000 / dist * dist);
+                Vector2 forse = dest * GravityConst * ((body.mass * Obj.GetComponent<Rigidbody2D>().mass) / dist * dist);
                 Obj.GetComponent<Rigidbody2D>().AddForce(-forse);
-
+                forses = forse.magnitude;
             }
         }
     }
